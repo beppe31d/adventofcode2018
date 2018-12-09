868425d6b35dbe897fcd1284b7b818644cb4549c
Giuseppe Del Gaudio <beppe31@gmail.com>
2018-12-09 21:10:12 +0100

    Day 8

--- day.php
+++ day.php
@@ -1,6 +1,9 @@
 <?php
 include_once 'vendor/autoload.php';
 
+$start = microtime(true);
 $inputs = file('./input/day' . $argv[1] . '.input');
 $class = 'AdventOfCode\Device\Day' . $argv[1];
 (new $class($inputs))->exec();
+$time = microtime(true) - $start;
+echo "\n\n" . 'Total time: ' . $time;

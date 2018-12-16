4df9792011c18e1bdb7983e35897d419603fd0ef
Giuseppe Del Gaudio <beppe31@gmail.com>
2018-12-16 12:36:38 +0100

    Day 14 part 2

--- src/Device/Day14.php
+++ src/Device/Day14.php
@@ -16,6 +16,9 @@ class Day14 extends AbstractDay
     {
         echo 'What are the scores of the ten recipes immediately after the number of recipes in your puzzle input? '
             . $this->getTenRecipes(919901);
+        echo "\n\n";
+        echo 'How many recipes appear on the scoreboard to the left of the score sequence in your puzzle input? '
+            . $this->getRecipesBackwards(919901);
     }
 
     /**
@@ -37,6 +40,37 @@ class Day14 extends AbstractDay
             if (\count($recipes) >= $size + 10) {
                 return \substr(implode('', $recipes), $size, 10);
             }
+        }
+    }
+
+    /**
+     * @param string $search
+     * @return int
+     */
+    private function getRecipesBackwards(string $search): int
+    {
+        $recipes = [3, 7];
+        $elves = [0, 1];
+        $recipeNumber = 0;
+        while (true) {
+            $newRecipe = $recipes[$elves[0]] + $recipes[$elves[1]];
+            $recipes[] = (int)\substr($newRecipe, 0, 1);
+            if (\strlen($newRecipe) === 2) {
+                $recipes[] = (int)\substr($newRecipe, -1);
+            }
+            $elves[0] = $this->getNextPosition($elves[0], $recipes);
+            $elves[1] = $this->getNextPosition($elves[1], $recipes);
+            if ($recipeNumber % 1000 !== 0) {
+                $recipeNumber++;
+                continue;
+            }
+
+            echo 'Recipe number ' . $recipeNumber . "\n";
+            $position = \strpos(implode('', $recipes), $search);
+            if (false !== $position) {
+                return $position;
+            }
+            $recipeNumber++;
         }
     }
 

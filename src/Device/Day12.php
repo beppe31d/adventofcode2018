b92584950ea93caffbac8858dda2c23d5dc7de01
Giuseppe Del Gaudio <beppe31@gmail.com>
2018-12-17 10:17:30 +0100

    Day 12 fix

--- src/Device/Day12.php
+++ src/Device/Day12.php
@@ -30,25 +30,42 @@ class Day12 extends AbstractDay
      */
     private function sumPlants(string $state, int $generations): int
     {
-        $plants = $this->getPlantsInGeneration($state);
-        echo $this->getPlantsInGeneration($state) . ' --- ' . $state . "\n";
-
+        $index = 0;
         for ($g = 0; $g < $generations; $g++) {
             $state = $this->getNextGeneration($state);
-            $plants += $this->getPlantsInGeneration($state);
-            echo $this->getPlantsInGeneration($state) . ' --- ' . $state . "\n";
-        }
 
-        return $plants;
+            $index -= 2;
+            $empty = '.....';
+            if (0 === \strpos($state, $empty)) {
+                $state = \substr($state, 4);
+                $index += 4;
+            }
+
+            if (\substr($state, -5) === $empty) {
+                $state = \substr($state, 0, -4);
+            }
+        }
+        echo $state . "\n";
+
+        return $this->getPlantsInGeneration($state, $index);
     }
 
     /**
      * @param string $state
+     * @param int $index
      * @return int
      */
-    private function getPlantsInGeneration(string $state): int
+    private function getPlantsInGeneration(string $state, int $index): int
     {
-        return \substr_count($state, '#');
+        $result = 0;
+        foreach (\str_split($state) as $pot) {
+            if ($pot === '#') {
+                $result += $index;
+            }
+            $index++;
+        }
+
+        return $result;
     }
 
     /**
@@ -62,15 +79,6 @@ class Day12 extends AbstractDay
         $state = '..' . $state . '..';
         for ($i = 0; $i <= $length; $i++) {
             $nextState .= $this->hasPattern(\substr($state, $i, 5)) ? '#' : '.';
-        }
-
-        $empty = '.....';
-        if (0 === \strpos($nextState, $empty)) {
-            $nextState = \substr($nextState, 4);
-        }
-
-        if (\substr($nextState, -5) === $empty) {
-            $nextState = \substr($nextState, 0, -4);
         }
 
         return $nextState;
